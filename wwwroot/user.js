d883bce08fa778c5f71f87f890ad0e5165b759d9
Hadassa Menucha <9760266@gmail.com>
2023-02-02 23:24:41 +0200

    client-done

--- wwwroot/user.js
+++ wwwroot/user.js
@@ -1,47 +1,48 @@
-class User{
-    id;name; password; isAdmin;
+class User {
+    id; name; password; isAdmin;
 }
-class ToDo{
+class ToDo {
     id; userid; name; isdone;
 }
 let tasks;
 let token;
 
-getToken=()=>{
+getToken = () => {
     return sessionStorage.getItem("Token");
 }
 
 const getAll = () => {
-    document.getElementById('container').innerHTML="";
-    
+    document.getElementById('container').innerHTML = "";
+
     fetch("https://localhost:5001/ToDo", {
         method: 'GET',
         headers: {
-            "Authorization": "Bearer "+getToken(),
+            "Authorization": "Bearer " + getToken(),
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         }
     })
-    .then(res=>res.json())
-    .then(result=>{result.forEach(td => {
-        const div = document.createElement('div');
-        const br = document.createElement('hr');
-        const p = document.createElement('p');
-        const p2=document.createElement('p')
-        p.innerHTML = 'To Do id:' + td.id;
-        const h2 = document.createElement('h2');
-        h2.innerHTML = 'To Do name:' + td.name;
-        p2.innerHTML='is done? '+td.isDone;
+        .then(res => res.json())
+        .then(result => {
+            result.forEach(td => {
+                const div = document.createElement('div');
+                const br = document.createElement('hr');
+                const p = document.createElement('p');
+                const p2 = document.createElement('p')
+                p.innerHTML = 'To Do id:' + td.id;
+                const h2 = document.createElement('h2');
+                h2.innerHTML = 'To Do name:' + td.name;
+                p2.innerHTML = 'is done? ' + td.isDone;
 
-        div.append(br, p, h2,p2);
+                div.append(br, p, h2, p2);
 
-        document.getElementById('container').append(div);
-    });
-    })
-    .catch(error => console.error('Unable to update item.', error));
+                document.getElementById('container').append(div);
+            });
+        })
+        .catch(error => console.error('Unable to update item.', error));
 
-           
-        
+
+
 }
 
 getAll()
@@ -55,13 +56,13 @@ add.onclick = () => {
 const finalAdd = document.getElementById('finalAdd');
 const inpname = document.getElementById('name');
 finalAdd.onclick = () => {
-    let td =new ToDo();
-    td.name=inpname.value;
-    td.isdone=false;
+    let td = new ToDo();
+    td.name = inpname.value;
+    td.isdone = false;
     fetch("https://localhost:5001/ToDo", {
         method: 'POST',
         headers: {
-            "Authorization": "Bearer "+getToken(),
+            "Authorization": "Bearer " + getToken(),
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
@@ -81,14 +82,14 @@ del.onclick = () => {
     toDelete.style.visibility = 'initial';
 }
 
-const finalDelete=document.getElementById('finalDelete');
-finalDelete.onclick=()=>{
-    const id= document.getElementById('id').value;
+const finalDelete = document.getElementById('finalDelete');
+finalDelete.onclick = () => {
+    const id = document.getElementById('id').value;
 
     fetch("https://localhost:5001/ToDo", {
         method: 'DELETE',
         headers: {
-            "Authorization": "Bearer "+getToken(),
+            "Authorization": "Bearer " + getToken(),
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
@@ -102,73 +103,79 @@ finalDelete.onclick=()=>{
 
 }
 
-const update =document.getElementById('update');
-const toupdate= document.getElementById('toUpdate')
-update.onclick=()=>{
-    toupdate.style.visibility='initial';
+const update = document.getElementById('update');
+const toupdate = document.getElementById('toUpdate')
+update.onclick = () => {
+    toupdate.style.visibility = 'initial';
 }
-const finalUpdate=document.getElementById('finalUpdate');
-const updateid=document.getElementById('updateId');
-const updatename=document.getElementById('taskName');
-const isdone=document.getElementById('isDone');
-finalUpdate.onclick=()=>{
-    let td= new ToDo();
-    td.id=updateid.value;
-    td.name=updatename.value;
-    td.isdone=isdone.value;
+const finalUpdate = document.getElementById('finalUpdate');
+const updateid = document.getElementById('updateId');
+const updatename = document.getElementById('taskName');
+const isdone = document.getElementById('isDone');
+finalUpdate.onclick = () => {
+    let td = {
+        id: updateid.value,
+        name: updatename.value,
+        isdone: isdone.checked
+    }
+
 
     console.log(td);
 
     fetch("https://localhost:5001/ToDo", {
         method: 'PUT',
         headers: {
-            "Authorization": "Bearer "+getToken(),
+            "Authorization": "Bearer " + getToken(),
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(td) 
+        body: JSON.stringify(td)
     })
-    .then(res=>getAll())
-    .catch(err=>console.log(err))
+        .then(res =>{ getAll();
+            updateid.value = '';
+            updatename.value = '';
+            isdone.value = '';
+            toupdate.style.visibility = 'hidden';
+        })
+        .catch(err => { console.log(err); })
 }
 
 
-const certainToDo=document.getElementById('certainToDo');
-const toCertainId=document.getElementById('toCertainId');
-const taskid=document.getElementById('taskid');
-const showCertain=document.getElementById('showCertain');
+const certainToDo = document.getElementById('certainToDo');
+const toCertainId = document.getElementById('toCertainId');
+const taskid = document.getElementById('taskid');
+const showCertain = document.getElementById('showCertain');
 
-certainToDo.onclick=()=>{
-    toCertainId.style.visibility='initial';
+certainToDo.onclick = () => {
+    toCertainId.style.visibility = 'initial';
 }
-showCertain.onclick=()=>{
+showCertain.onclick = () => {
     id = taskid.value;
 
-    fetch("https://localhost:5001/ToDo/"+id, {
+    fetch("https://localhost:5001/ToDo/" + id, {
         method: 'GET',
         headers: {
-            "Authorization": "Bearer "+getToken(),
+            "Authorization": "Bearer " + getToken(),
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         // body: JSON.stringify(td)
     })
-    .then(res=>res.json())
-    .then((td)=>
-        {
-            document.getElementById('showContainer').innerHTML='';
+        .then(res => res.json())
+        .then((td) => {
+            document.getElementById('showContainer').innerHTML = '';
             const div = document.createElement('div');
             const br = document.createElement('hr');
             const p = document.createElement('p');
-            const p2=document.createElement('p')
+            const p2 = document.createElement('p')
             p.innerHTML = 'To Do id:' + td.id;
             const h2 = document.createElement('h2');
             h2.innerHTML = 'To Do name:' + td.name;
-            p2.innerHTML='is done? '+td.isDone;
-    
-            div.append(br, p, h2,p2);
-    
+            p2.innerHTML = 'is done? ' + td.isDone;
+
+            div.append(br, p, h2, p2);
+
             document.getElementById('showContainer').append(div);
         }
-    )
+        )
 }
